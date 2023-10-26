9c257ac3843d1a57baae23a7eec9d262d0a6788d
farchatul07 <145920038+farchatul07@users.noreply.github.com>
2023-10-26 12:38:31 +0700

    Add files via upload

---
+++ src/main/java/com/mycompany/latihan18/Latihan18.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.latihan18;
+
+/**
+ *
+ * @author user
+ */
+public class Latihan18 {
+
+    public static void main(String[] args) {
+        for (double i = 3.5; i <= 35; i += 3.5) {
+            System.out.println(i);
+        }
+    }
+}
